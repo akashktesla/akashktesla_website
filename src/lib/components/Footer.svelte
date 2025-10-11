aa24960ef6414b69e4d1c4d50cc0d77a16e15c5d
akashktesla <akashktesla@gmail.com>
2025-10-12 01:06:38 +0530

    :)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,6 +6,6 @@
 <footer style=" color: #888; font-size: 0.9rem; text-align: center;">
   <p>&copy; 2025 Akash Tesla</p>
   <p>
-    <a href="https://github.com/akashtesla" target="_blank" style="color:#888; text-decoration:underline;">GitHub</a>
+    <a href="https://github.com/akashktesla" target="_blank" style="color:#888; text-decoration:underline;">GitHub</a>
   </p>
 </footer>
